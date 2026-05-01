9bb1adcc15d14cea19a424e937d253c3474d9d92
Dwuds <dawfw7399@gmail.com>
2026-05-01 17:12:58 +0700

    Tambahan Border

--- Assets/Script/input/PlayerMovement.cs
+++ Assets/Script/input/PlayerMovement.cs
@@ -6,16 +6,17 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 5f;
 
     private Vector2 moveDir;
+    private Rigidbody2D rb;
 
-    // Dipanggil oleh PlayerInput.onMove via UnityEvent
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void OnMove(Vector2 dir) {
         moveDir = dir;
     }
 
-    private void Update() {
-        if (moveDir == Vector2.zero) return; // skip kalau tidak ada input
-
-        Vector3 movement = new Vector3(moveDir.x, moveDir.y, 0f);
-        transform.Translate(movement * speed * Time.deltaTime);
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
     }
 }
